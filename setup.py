921a84f33de34d2faa0bc5e96e1f227fa87ad732
Cenk <cenk.math@gmail.com>
2015-01-19 19:50:11 +0200

    setup.py added

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='clustering',
+    version='0.1.1',
+    author='',
+    author_email='',
+    packages=find_packages(),
+    url='',
+    description='Clustering (Data Mining) implementation in Python',
+    long_description=open('README.md').read(),
+    install_requires=[
+        "enum >= 0.4.4",
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    license='MIT',
+    platforms=['any'],
+)
