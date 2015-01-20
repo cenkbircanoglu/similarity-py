0489ec04841eaccc5ab5f98a41df3088e357e438
Cenk <cenk.math@gmail.com>
2015-01-20 21:13:23 +0200

    version 0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='clustering',
-    version='0.1.1',
+    version='0.1.0',
     author='',
     author_email='',
     packages=find_packages(),
